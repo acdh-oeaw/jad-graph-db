26f76443554df3e75e89b72f7fefd4c02729de37
csae8092 <p.andorfer@gmail.com>
2026-02-27 09:10:37 +0100

    feat: persists similar texts

--- jad/admin.py
+++ jad/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from jad.models import JadRelation
+
+
+@admin.register(JadRelation)
+class JadRelation(admin.ModelAdmin):
+    list_display = ["id", "source_id", "target_id", "distance"]
--- jad/models.py
+++ jad/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class JadRelation(models.Model):
+    source_id = models.CharField(max_length=250, verbose_name="Source ID")
+    target_id = models.CharField(max_length=250, verbose_name="Target ID")
+    distance = models.FloatField(verbose_name="selected max distance")
+
+    class Meta:
+        ordering = ["-id"]
+        verbose_name = "Jad relation"
+        verbose_name_plural = "Jad relations"
+        unique_together = ("source_id", "target_id", "distance")
+
+    def __str__(self):
+        return f"{self.source_id}–{self.target_id} ({self.distance})"
