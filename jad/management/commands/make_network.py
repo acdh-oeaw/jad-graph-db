d53137be6220cc7541e0e20cd4c6610a0629b8ee
csae8092 <p.andorfer@gmail.com>
2026-02-27 09:10:37 +0100

    feat: persists similar texts

---
+++ jad/management/commands/make_network.py
@@ -0,0 +1,35 @@
+import time
+from datetime import timedelta
+
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from archiv.models import TextSnippet
+from jad.models import JadRelation
+
+
+class Command(BaseCommand):
+    help = "create network graph"
+
+    def handle(self, *args, **options):
+        start_time = time.time()
+        collection_title = "JAD sentences"
+        vector_field = "embedding_nomic"
+        amount = 3
+        max_distance = 0.1
+        items = TextSnippet.objects.filter(collection__title=collection_title)
+        for x in tqdm(items, total=items.count()):
+            source_id = x.text_id.split("-")[0]
+            for y in x.find_similar(
+                vector_field=vector_field,
+                collection_title=collection_title,
+                amount=amount,
+            ):
+                if y.distance < max_distance and y.text_id != x.text_id:
+                    target_id = y.text_id.split("-")[0]
+                    JadRelation.objects.get_or_create(
+                        source_id=source_id, target_id=target_id, distance=max_distance
+                    )
+
+        duration = time.time() - start_time
+        print(f"done in {timedelta(seconds=int(duration))}")
